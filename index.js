9305fa60cf7d82fdc7622b791b40f4486f5c81c4
Benjamin Horns <benhorns@gmail.com>
2019-12-29 05:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,6 @@ function placeOrder(cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
   getCart().length = 0;
-
   if (typeof cardNumber == `undefined`) {
     return `Sorry, we don't have a credit card on file for you.`}
   // write your code here
