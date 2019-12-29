a30259ac5ca524cfc76da299582b6f9af6da96b6
Benjamin Horns <benhorns@gmail.com>
2019-12-29 05:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (typeof cardNumber == `undefined`) {
     return "Sorry, we don't have a credit card on file for you."
-  }
+  } else {`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`}
   // write your code here
 }
